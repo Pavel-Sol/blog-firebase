8689d4e1be056a196af0d7fd419df293abb6c38e
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2021-06-26 21:43:05 +0400

    вывод одиночного поста на экран

--- src/components/pages/Post/Post.jsx
+++ src/components/pages/Post/Post.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
+import './Post.css'
 import {getCurrentPost} from './../../../store/actions/postActions'
 
 
@@ -16,7 +17,27 @@ const Post = (props) => {
    }, [])
    
 
-   return <h3>Post333</h3>
+   if(!currentPost) {
+      return <h1>загрузка</h1>
+   }
+
+   return (
+      <div className="_post-container _text-center">
+         {
+            currentPost.postImgLink &&
+            <div className="_post-img">
+               <img src={currentPost.postImgLink} alt="" />
+            </div>
+         }
+
+         <h1 className="_post-title">
+            {currentPost.heading}
+         </h1>
+         <p className="_post-text">
+            {currentPost.postText}
+         </p>
+      </div>
+   )
 }
 
 export default Post
