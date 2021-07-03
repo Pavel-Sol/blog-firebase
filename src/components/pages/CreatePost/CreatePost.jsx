902c1e4f6c93e1c3397e08b7a9de5153840613cc
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2021-07-03 13:54:10 +0400

    зануление формы отправки поста

--- src/components/pages/CreatePost/CreatePost.jsx
+++ src/components/pages/CreatePost/CreatePost.jsx
@@ -13,47 +13,53 @@ const [heading, setHeading] = useState('')
 const [postText, setPostText] = useState('')
 const [postImg,  setPostImg] = useState('')
 
-const handlePost =() => {
+const handlePost =(e) => {
+   e.preventDefault()
    dispatch(addPost(heading, postText, postImg, user.userName))
+   e.target.reset()
 }
 
    return (
       <div>
          <h3>добавьте новый пост</h3>
-         <div className="row">
-            <div className="input-field col s12">
-               <input onChange={(e) => setHeading(e.target.value)} 
-               id="heading" type="text" className="validate"/>
-               <label htmlFor="heading">Заголовок</label>
+         <form onSubmit={handlePost} action="">
+            <div className="row">
+               <div className="input-field col s12">
+                  <input 
+                  onChange={(e) => setHeading(e.target.value)} 
+                  id="heading" type="text" className="validate"/>
+                  <label htmlFor="heading">Заголовок</label>
+               </div>
             </div>
-         </div>
-         <div className="row">
-            <div className="input-field col s12">
-               <textarea onChange={(e) => setPostText(e.target.value)} 
-                id="textarea1" className="materialize-textarea"></textarea>
-               <label htmlFor="textarea1">Текст</label>
+            <div className="row">
+               <div className="input-field col s12">
+                  <textarea 
+                  onChange={(e) => setPostText(e.target.value)} 
+                  id="textarea1" className="materialize-textarea"></textarea>
+                  <label htmlFor="textarea1">Текст</label>
+               </div>
             </div>
-         </div>
-         <div className='row'>
-            <div className="col s12">
-               <div className="file-field input-field">
-                  <div className="btn">
-                     <span>прикрепить фото</span>
-                     <input onInput={(e) => setPostImg(e.target.files[0])} 
-                      type="file" />
-                  </div>
-                  <div className="file-path-wrapper">
-                     <input className="file-path validate" type="text" />
+            <div className='row'>
+               <div className="col s12">
+                  <div className="file-field input-field">
+                     <div className="btn">
+                        <span>прикрепить фото</span>
+                        <input onInput={(e) => setPostImg(e.target.files[0])} 
+                        type="file" />
+                     </div>
+                     <div className="file-path-wrapper">
+                        <input className="file-path validate" type="text" />
+                     </div>
                   </div>
                </div>
             </div>
-         </div>
-         <div className="row">
-            <div className="col s8 offset-s2 _center">
-               <a className="waves-effect waves-light btn-small"
-               onClick={handlePost}>добавить пост</a>
+            <div className="row">
+               <div className="col s8 offset-s2 _center">
+                  <button type='submit'
+                   className="waves-effect waves-light btn-small">добавить пост</button>
+               </div>
             </div>
-         </div>
+         </form>
       </div>
    )
 }
